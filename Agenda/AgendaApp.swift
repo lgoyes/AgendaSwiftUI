f1c3c1a3a62625ee45c3c7e4747dcf8d8f6c5d70
Luis Goyes <lgoyes@veritran.com>
2023-12-26 18:08:43 -0500

    Initial Commit

---
+++ Agenda/AgendaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AgendaApp.swift
+//  Agenda
+//
+//  Created by Luis David Goyes on 26/12/23.
+//
+
+import SwiftUI
+
+@main
+struct AgendaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
